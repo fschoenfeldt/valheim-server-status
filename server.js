d12d3c657dc95fffcd99f39bfae208ab2b3da3aa
Frederik Schönfeldt <fschoenfeldt@aidminutes.com>
2021-03-27 16:14:30 +0100

    add koa & gamedig test

---
+++ server.js
@@ -0,0 +1,16 @@
+const Koa = require('koa');
+const app = new Koa();
+const Gamedig = require('gamedig');
+
+
+app.use(async ctx => {
+    const response = await Gamedig.query({
+        type: 'protocol-valve',
+        host: 'localhost',
+        port: 2457,
+    })
+
+  ctx.body = response;
+});
+
+app.listen(3000);
